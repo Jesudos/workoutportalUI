32a49ddb817d3d38cff0a8b3dbd6544f8104b66c
Jesudos <jesudoss.jayakumar@cognizant.com>
2018-03-22 12:13:19 +0530

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpClientModule }    from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import {AppRoute} from './/app.routing.module';
+import { AlertService } from './alert.service';
+import { BaseurlService } from './baseurl.service';
+import { DataServiceService } from './data-service.service';
+import { UserDataService } from './user-data.service';
+import {RegisterService} from './register.service';
 
 
 @NgModule({
@@ -20,9 +25,17 @@ import {AppRoute} from './/app.routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoute
+    AppRoute,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ 
+    AlertService,
+    BaseurlService,
+    DataServiceService,
+    UserDataService,
+    RegisterService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
